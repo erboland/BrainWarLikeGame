aa3abb1484d26cc237be79b1d467ee3a887420fb
Ербол Каршыга <karshigaerbol@gmail.com>
2016-07-15 13:52:55 +0200

    Initial Commit

---
+++ Brainwar like game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Brainwar like game
+//
+//  Created by Ербол Каршыга on 7/15/16.
+//  Copyright © 2016 Yerbol Karshyga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
